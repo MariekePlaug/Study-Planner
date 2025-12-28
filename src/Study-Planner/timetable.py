5ab17f34c77ff025e8af850ac96a6dc75a6524f1
Marieke Plaug <marieke.plaug@studium.uni-hamburg.de>
2025-12-28 13:02:50 +0100

    added the days to the timetable, the mapping to x is now dynamically changing with changing figuresize, the yaxis changes dynamically with the times at which courses take place

--- src/Study-Planner/timetable.py
+++ src/Study-Planner/timetable.py
@@ -1,11 +1,11 @@
 # Imports
-import datetime as dt
-import matplotlib.dates as mdates
-from matplotlib.patches import Rectangle
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 import numpy as np
 import pandas as pd
 from pathlib import Path
+import matplotlib.dates as mdates
+from datetime import datetime, timedelta
 
 
 # Load File
@@ -25,48 +25,85 @@ df["start_time"] = pd.to_datetime(df["start_time"], format="%H:%M")
 df["duration"] = pd.to_timedelta(df["duration"], unit="minutes")
 df["end_time"] = df["start_time"] + df["duration"]
 
-# Period to create timetable display
-# period = Rectangle(
-#     xy=(df["day"], df["start_time"]),
-#     width=df["day"],
-#     height=df["duration"]
-# )
+# dynamic y-axis
+earliest_time = datetime(year = 1900, month = 1, day = 2, hour = 0, minute = 0)
+latest_time = datetime(year = 1900, month = 1, day = 1, hour = 0, minute = 0)
+for subject, row in df.iterrows():
+    starttime = row["start_time"]
+    endtime = row["end_time"]
+    if starttime < earliest_time:
+        earliest_time = starttime
+    if endtime > latest_time:
+        latest_time = endtime
+print(earliest_time, latest_time)
+
+y_bounds = [(earliest_time - timedelta(hours = 2)).hour, (latest_time + timedelta(hours = 2)).hour]
+print(y_bounds)
+
+yticks = np.arange(y_bounds[0]*60, y_bounds[1]*60 +1, 60)
+print(len(yticks))
+print(yticks)
+
+figsize_timetable = [8,6]
+height_ratios = [1,8]
+day_width = figsize_timetable[0] / len(WEEK_DAYS)
+text_offset = [day_width /2, height_ratios[0] / 2]
+
+fig = plt.figure(figsize = figsize_timetable)
+fig.subplots_adjust(left=0.1, right=0.95)
+gs = fig.add_gridspec(2, 1, height_ratios = height_ratios, hspace=0.0)
 
 
-# Plot
-fig, ax = plt.subplots(figsize=(8, 6))
-# for subject, row in df.iterrows():
-#     x = row["day"]
-#     y = row["start_time"]
-#     width = row["day"]
-#     height = row["duration"]
-#
-#     period = Rectangle(
-#         xy=(x, y),
-#         width=width,
-#         height=height
-#     )
-#     ax.add_patch(period)
-ax.set_title(f"{user}'s Study Timetable")
-ax.xaxis.tick_top()
-ax.set_xticks(ticks=np.arange(0, len(WEEK_DAYS)), labels=WEEK_DAYS)
-ax.set_xlim(- 0.5, len(WEEK_DAYS) - 0.5)
-ax.set_xticklabels(WEEK_DAYS)
-ax.set_xlabel("Week Day")
-ax.invert_yaxis()
-ax.yaxis.set_major_formatter(mdates.DateFormatter("%H:%M"))
-ax.yaxis.set_major_locator(mdates.HourLocator(byhour=np.arange(1, 24, 2)))
-ax.yaxis.set_minor_locator(mdates.MinuteLocator(interval=30))
-ax.set_ylabel("Hour")
-# ax.set_yticks(HOURS)
+
+ax1 = fig.add_subplot(gs[0])
+for i in range(len(WEEK_DAYS)):
+    rec = Rectangle( (i * day_width, 0), day_width, 1, edgecolor="black", facecolor="skyblue" )
+    ax1.add_patch(rec)
+
+    ax1.text(i * day_width + text_offset[0], text_offset[1], f"{WEEK_DAYS[i]}", ha="center", va="center", fontsize=12)
+
+ax1.set_xlim(0, figsize_timetable[0])
+ax1.set_ylim(0,1)
+ax1.axis("off")
+ax1.set_title(f"{user}'s Study Timetable \n", fontsize = 16)
+
+ax2 = fig.add_subplot(gs[1], sharex = ax1)
+ax2.set_yticks(yticks)
+ax2.set_ylim(yticks[0], yticks[-1])
+ax2.invert_yaxis()
+ax2.set_yticklabels([f"{int(h/60):02d}:00" for h in yticks])
+ax2.set_ylabel("Hour")
+ax2.set_xticks([])
+
+# plotting the courses:
+# Convert to mdates float values that change dynamically with changing figsize
+df["y"] = mdates.date2num(df["start_time"])
+df["height"] = mdates.date2num(df["end_time"]) - df["y"]
+
+# Map day names to x positions
+day_to_x = {day: i * figsize_timetable[0]/len(WEEK_DAYS) for i, day in enumerate(WEEK_DAYS)}
+df["x"] = df["day"].map(day_to_x)
+
+
+
+
+for subject, row in df.iterrows():
+    x = row["x"]
+    y = row["start_time"].hour * 60 + row["start_time"].minute  # Minutes since midnight
+    width = figsize_timetable[0] / len(WEEK_DAYS)  # One day wide
+    height = row["duration"].total_seconds() / 60  # Duration in minutes
+
+    period = Rectangle(
+        xy=(x, y),
+        width=width,
+        height=height,
+        facecolor="skyblue",
+        edgecolor="black",
+        label = subject
+    )
+    ax2.add_patch(period)
+    ax2.text(x + width*1/3, y + height*2/3, subject[0:6])
+ax2.legend()
 
 plt.show()
-
-# define times and days
-# start = dt.datetime(2025,1,1, 8,0)
-# datetime_vec = [start + i * dt.timedelta(minutes=15) for i in range(0,49)]  # quarter hour steps
-# time = [t.time() for t in datetime_vec]
-# print(time)
-# days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-# print(df)
 
